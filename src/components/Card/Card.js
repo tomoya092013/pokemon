f086203b62a5161b455e5f3da9d60eef0239b329
tomoya <cj.0s0ttw.nsm4@gmail.com>
2022-10-14 01:44:30 +0900

    レーダーチャート表示

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { PureComponent, useState } from "react";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  Legend,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from "recharts";
 
 const Card = ({ pokemon }) => {
+  const [hp, setHp] = useState("");
+  const [attack, setAttack] = useState("");
+  const [defense, setDefense] = useState("");
+  const [specialAttack, setSpecialAttack] = useState("");
+  const [specialDefense, setspecialDefense] = useState("");
+  const [speed, setSpeed] = useState("");
+  const data = [
+    { subject: "HP", A: pokemon.stats[0].base_stat, fullMark: 150 },
+    { subject: "こうげき", A: pokemon.stats[1].base_stat, fullMark: 150 },
+    { subject: "ぼうぎょ", A: pokemon.stats[2].base_stat, fullMark: 150 },
+    { subject: "とくこう", A: pokemon.stats[3].base_stat, fullMark: 150 },
+    { subject: "とくぼう", A: pokemon.stats[4].base_stat, fullMark: 150 },
+    { subject: "すばやさ", A: pokemon.stats[5].base_stat, fullMark: 150 },
+  ];
+
   return (
     <div className="card">
       <div className="cardImg">
@@ -38,6 +61,24 @@ const Card = ({ pokemon }) => {
           <p className="title">すばやさ：{pokemon.stats[5].base_stat}</p>
         </div>
       </div>
+      <RadarChart
+        cy={180}
+        outerRadius={150}
+        width={300}
+        height={350}
+        data={data}
+      >
+        <PolarGrid />
+        <PolarAngleAxis dataKey="pokemon" />
+        <PolarRadiusAxis angle={90} domain={[0, 120]} />
+        <Radar
+          name="ポケモンデータ"
+          dataKey="A"
+          stroke="#8884d8"
+          fill="red"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
     </div>
   );
 };
